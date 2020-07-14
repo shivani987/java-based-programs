d4c8ad81335db5dc7ff64af507cf7a75b656452f
shivani987 <shivanidikondwar9975@gmail.com>
2020-07-15 00:34:05 +0530

    modified

--- EmployeeWagePrograms/EmpWageUC3.java
+++ EmployeeWagePrograms/EmpWageUC3.java
@@ -5,32 +5,39 @@ public class EmpWageUC3
   {
    private static final int IS_PART_TIME=1;
    private static final int IS_FULL_TIME=2;
-   private static final int EMP_RATE_PER_HOUR=20;
 
     public static void main(String args[])
        {
-          int Salary=empCheck();
-          int TotalSalary= Salary * EMP_RATE_PER_HOUR;
-          System.out.println("Total Salary fo employee : " +TotalSalary);
-       }
-   public static final int empCheck()
-    {
-       // variables
-          int empHrs=0;
-        final int ranNumber = (int)(Math.random() * 10) % 2;
- 
-        if( IS_FULL_TIME == ranNumber)
-          {
-            empHrs=8; //if employee is fulltime employee
-          }
-        else if(IS_PART_TIME == ranNumber)
-          {
-            empHrs=4; //if employee is partTime employee
-          }
-        else
-          {
-           empHrs=0;
-          }
-          return empHrs;
-    }
+          // Calling function
+          final boolean isPresent=empCheck();
+          if(isPresent)
+           {
+           final double wage=calculateWage(20,8);
+           System.out.println("Employee is present");
+           System.out.println("Total Employee wage is :" +wage);
+            }
+           else
+            {
+             System.out.println("Total Employee wage is 0.0");
+            }
+      }
+  /**
+    * check employee 
+    * @return true if employee present otherwise false.
+    */
+      public static boolean empCheck()
+        {
+            return IS_PART_TIME == Math.floor(Math.random() * 10 ) % 2;
+        }
+ /**
+   * Calculate employee wage based on params.
+   * @param ratePerHrs the employee rate per hours.
+   * @param totalHrs total employee hours.
+   * @return total employee wage.
+   */
+     public static double calculateWage(final int ratePerHrs, final int totalHr)
+      {
+        return ratePerHrs * totalHr;
+      }
   }
+  
