9b59ad386880f3bf2c1a468059a0f5e74fd74fb0
shivani987 <shivanidikondwar9975@gmail.com>
2020-07-15 18:03:39 +0530

    updated

--- EmployeeWagePrograms/EmpWageUC1.java
+++ EmployeeWagePrograms/EmpWageUC1.java
@@ -7,11 +7,12 @@ public class EmpWageUC1
   private static final int IS_FULL_TIME=1;
   public static void main(String args[])
     {
-      empCheck();
+      empCheck(); // calling function
     }
   public static void empCheck()
     {
        double ranNumber = Math.floor(Math.random() * 10) % 2;
+        //  class returns present if employee is full time
         if(IS_FULL_TIME == ranNumber)
          {
             System.out.println("Employee is present");
